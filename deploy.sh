f983d1820bcf152a7a5fd41d5cbe6f72db4e943c
Richard Aubrey White <hello@rwhite.no>
2022-11-08 11:23:56 +0100

    Pushing fixes.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 reset
 Rscript -e "quarto::quarto_render('.')"
-cp -r articles favicon.png CNAME docs/
+cp -r favicon.png CNAME docs/
 cp -r resources/* docs/resources
 sleep 5
 git add docs && git commit -m "docs commit"
